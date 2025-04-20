133847cc9a5a805e10aeecbfa24f704883ca1fd3
ytalaat969 <161771456+ytalaat969@users.noreply.github.com>
2025-04-20 17:56:08 +0200

    Update The function.cpp

--- The function.cpp
+++ The function.cpp
@@ -5,16 +5,16 @@ int find (int num1, int num2, int num3)
     if (num1>num2 && num1>num3)
     {
 
-        cout << num1<< "is a maxmum num"<< endl;
+        cout << num1<< "is a maximum num"<< endl;
     }
     if (num2>num1 && num2>num3)
     {
 
-        cout << num2<< "is a maxmum num"<< endl;
+        cout << num2<< "is a maximum num"<< endl;
     }
 
     else {
-        cout << num3 << "is a max num" << endl;
+        cout << num3 << "is a maximum num" << endl;
     }
 }
 int main()
